7081bed2d9be437966035314fe9c8d4eb655d329
Miriyan Asenov <m.asenov.3377@gmail.com>
2024-01-24 16:23:28 +0200

    7 Create mongoose schema and model - Transfer

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const transferSchema = new mongoose.Schema({
+	amount: {type: Number, required: true},
+	receiver: {type: String, required: true},
+	sender: {type: String}
+});
+
+const Transfer = mongoose.model('Transfer', transferSchema); 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
